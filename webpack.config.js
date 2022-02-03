11d15cfee20ceaab41f6ee96b8bb62f6530fa307
Hefler <heflerdev@gmail.com>
2022-02-03 15:30:05 -0300

    Finish Config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+    mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader"
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg)$/i,
+                loader: "file-loader"
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    }
+};
